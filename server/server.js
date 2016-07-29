3266bb7f6752f9b762a83d48c116af5753b1fd83
jimliu <jim.liu@mitake.com.tw>
2016-07-29 17:49:51 +0800

    [add] server project [modify] index2.html 增加 lightbox 功能

---
+++ server/server.js
@@ -0,0 +1,38 @@
+﻿
+var sql = require('mssql');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/socket.html');
+});
+
+
+//指定port
+http.listen(process.env.PORT || 8001, function () {
+    console.log('listening on *:8001');
+});
+
+io.on('connection', function (socket) {
+    socket.on('user_login', function (data) {
+        socket.username = data.username;
+
+        var now = new Date();
+        var nowStr = now.toLocaleDateString() + ' ' + now.toLocaleTimeString();
+        console.log(nowStr + ' ' + data.username + ' Login!');
+    });
+
+
+    socket.on('chat_message', function (data) {
+        var now = new Date();
+        var nowStr = now.toLocaleDateString() + ' ' + now.toLocaleTimeString();
+        console.log(nowStr + ' ' + socket.username + ' ' + data.msg);
+
+        io.emit('chat_message', {
+            'time': nowStr,
+            'user': socket.username,
+            'msg': data.msg
+        });
+    });
+});
---
+++ server/typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/mssql/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/socket.io/index.d.ts" />
